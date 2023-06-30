6b6d12c857ec4af59a3c5b0716afab2a88d3487f
Douglas Araujo <douglasaraujosampaio@gmail.com>
2023-06-30 14:25:12 -0300

    classe travel

--- src/main/java/br/com/douglas/viagem/model/Travel.java
+++ src/main/java/br/com/douglas/viagem/model/Travel.java
@@ -1,5 +1,23 @@
 package br.com.douglas.viagem.model;
 
-public class Travel {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Travel {
+	
+	private Long id;
+	private String orderNumber;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
+	private BigDecimal amount;
+	//private TravelTypeEnum type;
+	
 }
